b904b18416ec33b1227d15a0f2741aec948e37d0
Juan José Arboleda <soyjuanarbol@gmail.com>
2018-10-18 16:53:18 -0500

    CRUD with controller and model working as expected

---
+++ api/controllers/UserController.js
@@ -0,0 +1,81 @@
+const User = require('../models/User')
+
+class UserController {
+  async create (data) {
+    try {
+      const createPromise = User.create(data)
+      const response = await createPromise
+      return response
+    } catch (err) {
+      process.nextTick(() => console.error(`Error creating user: ${err}`))
+      throw err
+    }
+  }
+
+  async register (data) {
+    const {
+      age,
+      marriageStatus,
+      name,
+      password,
+      username
+    } = data
+
+    const user = new User({
+      age,
+      marriageStatus,
+      name,
+      username
+    })
+
+    try {
+      const resgisterPromise = User.register(user, password)
+      const response = await resgisterPromise
+      return response
+    } catch (err) {
+      process.nextTick(() => console.error(`Error registering user: ${err}`))
+      throw err
+    }
+  }
+
+  async show (id) {
+    try {
+      const findPromise = User.findById(id)
+      const findResponse = await findPromise
+      return findResponse
+    } catch (err) {
+      process.nextTick(() => console.error(`Error getting user: ${err}`))
+      throw err
+    }
+  }
+
+  async update (id, data) {
+    try {
+      const updatePromise = User.findByIdAndUpdate(id, data)
+      const updateResponse = await updatePromise
+      return updateResponse
+    } catch (err) {
+      process.nextTick(() => console.error(`Error updating user: ${err}`))
+      throw err
+    }
+  }
+
+  async delete (id) {
+    try {
+      const deletePromise = User.findByIdAndDelete(id)
+      const response = await deletePromise
+      return response
+    } catch (err) {
+      process.nextTick(() => console.error(`Error deleting user: ${err}`))
+      throw err
+    }
+  }
+}
+
+// Make controller singleton
+let controller = null
+if (!controller) {
+  controller = new UserController()
+}
+
+module.exports = controller
--- api/models/User.js
+++ api/models/User.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 const passportLocalMongoose = require('passport-local-mongoose')
 
+const stringRequireValidator = {
+  required: true,
+  type: String
+}
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  name: String
-})
+  age: Number,
+  marriageStatus: String,
+  name: stringRequireValidator,
+  password: { ...stringRequireValidator, required: false },
+  username: stringRequireValidator
+}, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
 
 userSchema.plugin(passportLocalMongoose)
 
